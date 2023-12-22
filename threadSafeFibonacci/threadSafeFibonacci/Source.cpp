26060694961ac8f49473b35d317c02ad7053b5da
savicpmf <marko.savic@pmf.edu.rs>
2023-12-22 20:07:29 +0100

    first version
    first version using an atomic sum

---
+++ threadSafeFibonacci/threadSafeFibonacci/Source.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <vector>
+#include <functional>
+#include <algorithm>
+
+using namespace std;
+
+
+int fibonaci(int n)
+{
+	if (n <= 1)
+	{
+		return n;
+	}
+
+	else
+	{
+		return fibonaci(n - 1) + fibonaci(n-2);
+	}
+}
+
+void posao(int pocetak,int kraj,int radPoNit,atomic<int>& rezultat)
+{
+	for (int i = pocetak; i < kraj; i++)
+	{
+		//cout << "fibonaci [" << i << "] = " << fibonaci(i) << " ";
+		rezultat += fibonaci(i);
+	}
+}
+
+
+
+int main()
+{
+	atomic<int> rezultat;
+	int i, zbir = 0, brojNiti = 8, n = 10, start = 0, end = 0;
+	vector<thread>niti(brojNiti);
+
+	int radPoNit = (n+brojNiti-1)/brojNiti;
+
+	
+
+	for (i = 0; i < brojNiti-1; i++)
+	{
+		end = min(start + radPoNit, n);
+		niti[i] = thread(posao, start, end, radPoNit, ref(rezultat));
+		start = end;
+		if (start >= n)
+			break;
+	}
+	posao(start, n+1, radPoNit, rezultat);
+
+	//for_each(niti.begin(), find_if(niti.begin(), niti.end(), [](const thread& t) { return !t.joinable(); }), mem_fn(&thread::join));
+
+	for (auto& t : niti) {
+		if (t.joinable()) {
+			t.join();
+		}
+	}
+
+	cout << rezultat;
+	
+
+	return 0;
+}
